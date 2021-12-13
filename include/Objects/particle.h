2d29ec3f79c7698da36fe486873191c8530b28f4
Ben Williams <ben.williams14@outlook.com>
2021-12-13 10:27:21 -0500

    Added particle optimization

--- include/Objects/particle.h
+++ include/Objects/particle.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Control/scene_node.h"
+#include "Control/mathematics.h"
 
 namespace game {
 class Particle : public SceneNode {
@@ -9,11 +10,14 @@ public:
 	~Particle();
 
 	virtual void Update() override;
+	virtual void Draw(Camera* camera) override;
 
 	float GetVelocityMultiple();
 
 	void SetVelocityMultiple(float multiple);
 	void SetInstanceAmount(int amount); // set the instanceAmount value in scene_node
+
+	Math::SphereCollider GetCollider();
 
 private:
 	float velMultiple_;
--- src/Objects/particle.cpp
+++ src/Objects/particle.cpp
@@ -19,6 +19,14 @@ namespace game {
 	{
 	}
 
+	void Particle::Draw(Camera* camera)
+	{
+		// only draws particle if within set distance
+		if (Math::isCollidingSphereToSphere(Game::GetInstance().GetPlayer()->GetTank()->GetCollider(), GetCollider())) {
+			SceneNode::Draw(camera);
+		}
+	}
+
 	float Particle::GetVelocityMultiple()
 	{
 		return velMultiple_;
@@ -32,6 +40,12 @@ namespace game {
 	void Particle::SetInstanceAmount(int amount)
 	{
 		instanceAmount_ = amount;
+	}
+
+	Math::SphereCollider Particle::GetCollider()
+	{
+		// used to see if the particle should be drawn
+		return { GetPosition(), 150.0f };
 	}
 
 	void Particle::InitShaderUniform(GLuint program)
